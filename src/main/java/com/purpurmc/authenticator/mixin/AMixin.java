3226e081c93786068f730b90c14744911cf90de0
Hüseyin Serhat Han <59025999+UsainSrht@users.noreply.github.com>
2023-03-24 03:01:29 +0300

    configure dependencies

--- src/main/java/com/purpurmc/authenticator/mixin/AMixin.java
+++ src/main/java/com/purpurmc/authenticator/mixin/AMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class AMixin {
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
-        Authenticator.LOGGER.info("This line is printed by an example mod mixin!");
+        Authenticator.LOGGER.info("Mixin test message");
     }
 }
